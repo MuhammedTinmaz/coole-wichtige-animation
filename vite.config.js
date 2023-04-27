2f8a93c29f421d1e5357792e8e29ab618d69066e
Muhammed Tinmaz <113904646+MuhammedTinmaz@users.noreply.github.com>
2023-04-27 13:32:55 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/coole-wichtige-animation"
+  base: "/coole-wichtige-animation",
   plugins: [react()],
 })
